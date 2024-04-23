1163183536aefefbf8ead60b7b8f10956b947d78
Afnansayed <afnansayed1973@gmail.com>
2024-04-24 04:30:16 +0600

    clint site delete code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 
+import { useLoaderData } from 'react-router-dom'
 import './App.css'
+import Coffee from './components/Coffee';
 
 function App() {
-
+const coffee = useLoaderData();
 
   return (
     <>
-      <h3 className="text-4xl font-bold btn">Ok is work</h3>
+      <h3 className="text-4xl font-bold btn">Ok is work {coffee.length}</h3>
+      <div className='grid grid-col-1 lg:grid-cols-2 gap-5'>
+        {
+          coffee.map(newCoffee => <Coffee
+             key={newCoffee._id}
+             newCoffee={newCoffee}
+             ></Coffee>)
+        }
+      </div>
     </>
   )
 }
---
+++ src/components/Coffee.jsx
@@ -0,0 +1,62 @@
+import PropTypes from 'prop-types'
+import Swal from 'sweetalert2';
+
+const Coffee = ({ newCoffee }) => {
+    const { _id, name, quantity, supplier, photo } = newCoffee;
+
+    const handleDelete = _id => {
+        console.log(_id)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                //console.log('delete successful')
+                fetch(`http://localhost:5000/coffee/${_id}`,{
+                    method: 'delete'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                              Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                              });
+                        }
+                    })
+            }
+        });
+    }
+    return (
+        <div className="flex  bg-base-100 shadow-xl">
+            <figure><img src={photo} alt="Album" className='w-[80%]' /></figure>
+            <div className="flex justify-between border-red-100 border-1 w-full">
+                <div>
+                    <h2 className="text-xl font-bold">{name}</h2>
+                    <h2 className="font-bold">{quantity}</h2>
+                    <h2 className="font-bold">{supplier}</h2>
+                </div>
+                <div className="card-actions justify-end">
+                    <div className="join join-vertical">
+                        <button className="btn join-item">view</button>
+                        <button className="btn join-item">Edit</button>
+                        <button
+                            onClick={() => handleDelete(_id)}
+                            className="btn join-item">X</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+Coffee.propTypes = {
+    newCoffee: PropTypes.object
+}
+export default Coffee;
